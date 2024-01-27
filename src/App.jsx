d5194a7b11bddca1982bf04dd432b90c09844678
Andy Soloman <andyjsoloman@gmail.com>
2024-01-26 20:17:01 -0800

    camera, position, fov

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import { OrbitControls } from "@react-three/drei";
 function App() {
   return (
     <>
-      <Canvas>
+      <Canvas
+        orthographic
+        camera={{ fov: 100, zoom: 100, position: [0, 0, 8] }}
+      >
         <OrbitControls />
         <directionalLight position={[1, 2, 3]} />
-        <FullFrame></FullFrame>
+        <FullFrame scale={0.1} position={[-2, -1, 0]} />
       </Canvas>
     </>
   );
