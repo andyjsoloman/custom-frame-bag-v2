968ec795a2830e7156b66b4051dabd21d669d68e
Andy Soloman <andyjsoloman@gmail.com>
2024-01-27 20:48:09 -0800

    add light

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -7,7 +7,9 @@ import FullFrame from "./FullFrame";
 
 export default function Experience() {
   const directionalLight = useRef();
+  const directionalLight2 = useRef();
   useHelper(directionalLight, THREE.DirectionalLightHelper, 1);
+  useHelper(directionalLight2, THREE.DirectionalLightHelper, 1);
 
   return (
     <>
@@ -22,8 +24,15 @@ export default function Experience() {
         ref={directionalLight}
         castShadow
         shadow-mapSize={[1024, 1024]}
-        position={[1, 5, 8]}
+        position={[1, 6, 8]}
         intensity={2.5}
+      />
+      <directionalLight
+        ref={directionalLight2}
+        castShadow
+        shadow-mapSize={[1024, 1024]}
+        position={[1, 6, -8]}
+        intensity={1.5}
       />
       <ambientLight intensity={0.5} />
       <FullFrame scale={0.1} position={[-2, -1, 0]} />
